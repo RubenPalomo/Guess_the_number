17c18155f74db3a0f72abd37ce4364c006c546d7
Rubén Palomo Fontán <ruben.palomof@gmail.com>
2023-11-21 13:32:27 +0100

    Fixed bug when saving player data

--- util/sentToTelegram.ts
+++ util/sentToTelegram.ts
@@ -1,15 +1,27 @@
 import axios from "axios";
+import { TELEGRAM_TOKEN, TELEGRAM_ID } from "@env";
+import { getToken } from "./getToken";
 import SendAlert from "./SendAlert";
 import IUser from "../types/IUser";
-import { TELEGRAM_TOKEN, TELEGRAM_ID } from "@env";
+import IPlayer from "../types/IPlayer";
 
 export const sendToTelegram = async (user: IUser): Promise<void> => {
-    axios
-        .post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
-            chat_id: TELEGRAM_ID,
-            text: user,
-        })
-        .catch((error: any) => {
-            SendAlert("Error al enviar el mensaje", error);
-        });
+    const token: string | undefined = await getToken();
+
+    if (token !== undefined) {
+        const player: IPlayer = {
+            token: token,
+            name: user.name,
+            record: user.record,
+        };
+
+        axios
+            .post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+                chat_id: TELEGRAM_ID,
+                text: player,
+            })
+            .catch((error: any) => {
+                SendAlert("Error al enviar el mensaje", error);
+            });
+    }
 };
