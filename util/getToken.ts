9d29088be7add8bd69ad787c486ef823c01a8b97
Rubén Palomo Fontán <ruben.palomof@gmail.com>
2023-11-24 23:08:08 +0100

    Fixed bug sending data to bot

--- util/getToken.ts
+++ util/getToken.ts
@@ -3,33 +3,35 @@ import * as Device from "expo-device";
 import { Platform } from "react-native";
 
 export const getToken = async (): Promise<string> => {
-    const deviceType = await Device.getDeviceTypeAsync();
-    let token: string = `${deviceType}:${Device.modelId}:${Device.brand}:${Device.designName}`;
+    let token: string = `${Device.deviceType}:${Device.modelId}:${Device.brand}:${Device.designName}`;
 
-    if (Platform.OS === "android") {
-        await Notifications.setNotificationChannelAsync("default", {
-            name: "default",
-            importance: Notifications.AndroidImportance.MAX,
-            vibrationPattern: [0, 250, 250, 250],
-            lightColor: "#FF231F7C",
-        });
-    }
-
-    if (Device.isDevice) {
-        const { status: existingStatus } =
-            await Notifications.getPermissionsAsync();
-
-        let finalStatus = existingStatus;
-
-        if (existingStatus !== "granted") {
-            const { status } = await Notifications.requestPermissionsAsync();
-            finalStatus = status;
+    try {
+        if (Platform.OS === "android") {
+            await Notifications.setNotificationChannelAsync("default", {
+                name: "default",
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: "#FF231F7C",
+            });
         }
 
-        if (finalStatus !== "granted") return token;
+        if (Device.isDevice) {
+            const { status: existingStatus } =
+                await Notifications.getPermissionsAsync();
 
-        token = (await Notifications.getExpoPushTokenAsync()).data;
-    }
+            let finalStatus = existingStatus;
+
+            if (existingStatus !== "granted") {
+                const { status } =
+                    await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+
+            if (finalStatus !== "granted") return token;
+
+            token = (await Notifications.getExpoPushTokenAsync()).data;
+        }
+    } catch (error: any) {}
 
     return token;
 };
